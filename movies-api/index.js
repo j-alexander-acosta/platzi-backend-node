53912c05efb1bc4ed8993b7dd7178657b8a3ce6b
J. Alexander Acosta Z <77844549+j-alexander-acosta@users.noreply.github.com>
2021-10-20 22:12:42 -0300

    Add files

---
+++ movies-api/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+const { config } = require('./config/index');
+const moviesApi = require('./routes/movies.js');
+
+const {
+    logErrors,
+    wrapErrors,
+    errorHandler,
+} = require('./utils/middleware/errorHandlers');
+
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
+
+// body parser
+app.use(express.json());
+
+// routes
+moviesApi(app);
+
+// Catch 404
+app.use(notFoundHandler);
+
+// Errores middleware
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandler);
+
+app.listen(config.port, function() {
+    console.log(`Listening http://localhost:${config.port}`);
+});
